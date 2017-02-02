8a65cf51161c00a9af57230f2c5a946485c60501
3rawkz <e.rawkz@gmail.com>
2017-02-02 13:04:23 -0600

    First commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
@@ -7,7 +7,7 @@ NEWSPIDER_MODULE = "{{cookiecutter.first_spider_name}}.spiders"
 LOG_FILE = "history.log"
 
 # Do not obey robots.txt
-ROBOTSTXT_OBEY = "{{cookicutter.obey_robotxt}}"
+ROBOTSTXT_OBEY = "{{cookiecutter.obey_robotxt}}"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) (KHTML, like Gecko) Version/2.1"
